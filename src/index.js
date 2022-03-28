608f9d62bbeab63b68d67566cddce386d35d5ea2
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-03-28 00:33:56 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-classes-per-file */
+
+import './style.scss';
+import PageManager from './modules/pagemanager.js';
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
+import '@fortawesome/fontawesome-free/js/regular.js';
+import '@fortawesome/fontawesome-free/js/brands.js';
+
+const pageManager = new PageManager();
+pageManager.pageSetup();
